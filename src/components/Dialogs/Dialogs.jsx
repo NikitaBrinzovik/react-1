2042627249310c8e91e738c7da80ce86e318c92a
Nikita <knanaprimer@gmail.com>
2020-12-27 02:35:36 +0300

    UI-BLL. hardcore massive)))

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,26 +6,48 @@ import Message from './Message/Message';
 
 
 const Dialogs = (props) => {
+
+    let dialogsData =[
+        {id: 1, name: 'Dima'},
+        {id: 2, name: 'Kirill'},
+        {id: 3, name: 'Sasha'},
+        {id: 4, name: 'Masha'},
+        {id: 5, name: 'Kos'},
+        {id: 6, name: 'Nikita'},
+        {id: 7, name: 'Viktoria'},
+        {id: 8, name: 'Polina'},
+    ]
+
+    let messagesData = [
+        {id: 1, text:'privet', touched:'no'},
+        {id: 2, text:'ne pichi mne bolche', touched:'no'},
+        {id: 3, text:'kysay ego', touched:'no'},
+        {id: 4, text:'nichego', touched:'no'},
+        {id: 5, text:'poka', touched:'no'},
+        {id: 6, text:'ffffffffffff', touched:'no'}
+    ]
+
+
     return (
         <div className={s.allChats}>
             <div className={s.allDialogs}>
-                <Dialog name={'Dima'} id={'1'}/>
-                <Dialog name={'Kirill'} id={'2'}/>
-                <Dialog name={'Sasha'} id={'3'}/>
-                <Dialog name={'Viktor'} id={'4'}/>
-                <Dialog name={'Kos'} id={'5'}/>
-                <Dialog name={'Nikita'} id={'6'}/>
-                <Dialog name={'Viktoria'} id={'7'}/>
-                <Dialog name={'Polina'} id={'8'}/>
+                <Dialog name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <Dialog name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <Dialog name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <Dialog name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <Dialog name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <Dialog name={dialogsData[5].name} id={dialogsData[5].id}/>
+                <Dialog name={dialogsData[6].name} id={dialogsData[6].id}/>
+                <Dialog name={dialogsData[7].name} id={dialogsData[7].id}/>
             </div>
 
             <div className={s.messages}>
-                <Message text={'privet'} touched={'no'}/>
-                <Message text={'ne pichi mne bolche'} touched={'no'}/>
-                <Message text={'kysay ego'} touched={'no'}/>
-                <Message text={'nichego'} touched={'no'}/>
-                <Message text={'poka'} touched={'no'}/>
-                <Message text={'ffffffffffff'} touched={'no'}/>
+                <Message text={messagesData[0].text} touched={messagesData[0].touched}/>
+                <Message text={messagesData[1].text} touched={messagesData[1].touched}/>
+                <Message text={messagesData[2].text} touched={messagesData[2].touched}/>
+                <Message text={messagesData[3].text} touched={messagesData[3].touched}/>
+                <Message text={messagesData[4].text} touched={messagesData[4].touched}/>
+                <Message text={messagesData[5].text} touched={messagesData[5].touched}/>
             </div>
         </div>
     );
--- src/components/profile/myPosts/myPosts.jsx
+++ src/components/profile/myPosts/myPosts.jsx
@@ -2,6 +2,18 @@ import s from './myPosts.module.css'; //можно classes заменить лю
 import Post from './Posts/Post';
 
 const MyPosts = () => {
+
+    let postsData =[
+        {id: 1, message: 'Hi, how are you?', numb:122},
+        {id: 2, message: 'It\'s my first post', numb:2},
+        {id: 3, message: 'Sasha', numb:22},
+        {id: 4, message: 'Masha', numb:3},
+        {id: 5, message: 'Kos', numb:1},
+        {id: 6, message: 'Nikita', numb:333},
+        {id: 7, message: 'Viktoria', numb:2},
+        {id: 8, message: 'Polina', numb:11}
+    ]
+
     return (
         <div className={s.postBlock}>
             <h3>My Posts</h3>
@@ -12,8 +24,14 @@ const MyPosts = () => {
                 <button>remove post</button>
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' numb='122'/>
-                <Post message="It's my first post" numb='12'/>
+                <Post message={postsData[0].message} numb={postsData[0].numb}/>
+                <Post message={postsData[1].message} numb={postsData[1].numb}/>
+                <Post message={postsData[2].message} numb={postsData[2].numb}/>
+                <Post message={postsData[3].message} numb={postsData[3].numb}/>
+                <Post message={postsData[4].message} numb={postsData[4].numb}/>
+                <Post message={postsData[5].message} numb={postsData[5].numb}/>
+                <Post message={postsData[6].message} numb={postsData[6].numb}/>
+                <Post message={postsData[7].message} numb={postsData[7].numb}/>
             </div>
         </div>
     );
