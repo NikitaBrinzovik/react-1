64617639161db85c1c5d7ac333d1d9d1f1dcfc95
Nikita <knanaprimer@gmail.com>
2021-01-08 00:16:06 +0300

    lesson31 in "myPosts": ref, onClick, VirtualDOM

--- src/components/profile/myPosts/myPosts.jsx
+++ src/components/profile/myPosts/myPosts.jsx
@@ -6,16 +6,26 @@ import React from "react";
 
 const MyPosts = (props) => {
 
-    let postsElements = props.postsData.map(p => <Post message={p.message} numb={p.numb} id={p.id}/>);
+    let postsElements =
+        props.postsData.map(p => <Post message={p.message} numb={p.numb} id={p.id}/>);
 
+    let newPostEl= React.createRef()
+
+    let addPost = () => {
+        let text= newPostEl.current.value;
+        alert('hey, you made new post:'+' "'+text+'"')
+
+    }
 
     return (
         <div className={s.postBlock}>
             <h3>My Posts</h3>
             <div>
                 <h4>new post: </h4>
-                <div><textarea></textarea></div>
-                <button>send post</button>
+                <div>
+                    <textarea ref={newPostEl}></textarea>
+                </div>
+                <button onClick={addPost}>send post</button>
                 <button>remove post</button>
             </div>
             <div className={s.posts}>
